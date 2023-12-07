57a9a3524e3b285ff88201e777dfa84bc428d882
André Rocha <d3roch4@gmail.com>
2023-12-07 14:42:57 -0300

    Atualizando repo

---
+++ modules/account/lib/account.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/entites/account.dart';
+export 'src/services/account_service.dart';
+export 'src/services/auth0_account_service.dart';
---
+++ modules/core/lib/core.dart
@@ -0,0 +1,10 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/entities/entity.dart';
+export 'src/local_storage/local_storage.dart';
+export 'src/local_storage/local_storage_shared_preference_impl.dart';
+
+// TODO: Export any libraries intended for clients of this package.
